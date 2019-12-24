0381460e7e24f38a45a1527f5922db65d077adcf
FlupkeZwe <frits@zwegers.net>
2019-12-24 10:16:13 +0100

    Fixed channel selector

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ This includes:
 In the Netherlands this type of settopbox is used by T-Mobile Thuis.
 
 Version 1.0.2 fixes a resource leak when sending signals.
+Also the channel selector is repaired using enumeration for channels 0-99
+to be used in the homey app and a text-field accepting a number for the flowcards.
--- drivers/entone_remote/device.js
+++ drivers/entone_remote/device.js
@@ -3,31 +3,31 @@
 const Homey = require('homey');
 
 module.exports = class EntoneRemoteDevice extends Homey.Device {
-
     onInit() {
-        let entoneSignal = this._initSignal();
+        this.entoneSignal = this._initSignal();
 
 
-        this.registerCapabilityListener('onoff', () => {return this._performCommand(entoneSignal, 'POWER_TOGGLE');});
-        this.registerCapabilityListener('channel_up', () => {return this._performCommand(entoneSignal, 'CHANNEL_UP');});
-        this.registerCapabilityListener('channel_down', () => {return this._performCommand(entoneSignal, 'CHANNEL_DOWN');});
-        this.registerCapabilityListener('volume_up', () => {return this._performCommand(entoneSignal, 'VOLUME_UP');});
-        this.registerCapabilityListener('volume_down', () => {return this._performCommand(entoneSignal, 'VOLUME_DOWN');});
+        this.registerCapabilityListener('onoff', () => {return this._performCommand(this.entoneSignal, 'POWER_TOGGLE');});
+        this.registerCapabilityListener('channel_up', () => {return this._performCommand(this.entoneSignal, 'CHANNEL_UP');});
+        this.registerCapabilityListener('channel_down', () => {return this._performCommand(this.entoneSignal, 'CHANNEL_DOWN');});
+        this.registerCapabilityListener('volume_up', () => {return this._performCommand(this.entoneSignal, 'VOLUME_UP');});
+        this.registerCapabilityListener('volume_down', () => {return this._performCommand(this.entoneSignal, 'VOLUME_DOWN');});
 
         this.registerCapabilityListener('channel_switch', () => {return this._switchChannel(entoneSignal, value);});
+        this.registerCapabilityListener('channel_switch', this._switchChannel.bind(this));
         let channelSwitchAction = new Homey.FlowCardAction('channel_switch');
         channelSwitchAction
             .register()
             .registerRunListener(( args ) => {
-                return this._switchChannel(entoneSignal, args['channel']);
+                return this._switchChannel(args['channel']);
             });
 
-        this.registerCapabilityListener('volume_mute', () => {return this._performCommand(entoneSignal, 'MUTE_TOGGLE');});
+        this.registerCapabilityListener('volume_mute', () => {return this._performCommand(this.entoneSignal, 'MUTE_TOGGLE');});
         let volumeMuteAction = new Homey.FlowCardAction('volume_mute');
         volumeMuteAction
             .register()
             .registerRunListener(( ) => {
-                return this._performCommand(entoneSignal, 'MUTE_TOGGLE')
+                return this._performCommand(this.entoneSignal, 'MUTE_TOGGLE')
             });
     }
 
@@ -50,14 +50,14 @@ module.exports = class EntoneRemoteDevice extends Homey.Device {
         return Promise.resolve(true);
     }
 
-    _switchChannel(entoneSignal,value) {
+    _switchChannel(value) {
         if (isNaN(value)) {
             this.log('Received a non-number', value);
             return Promise.reject('NaN');
         }
         const channelArray = value.toString().split('');
         channelArray.forEach( (digit) => {
-            this._performCommand(entoneSignal, 'DIGIT_' + digit);
+            this._performCommand(this.entoneSignal, 'DIGIT_' + digit);
         });
         return Promise.resolve(true);
     }
